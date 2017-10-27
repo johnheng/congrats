1954f9f8a28cec464900a8390abbfc5e53407b6c
John Heng <jheng@retaildatallc.net>
2017-10-27 14:49:54 -0400

    Component set up

---
+++ src/app/comment-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'comments',
+  templateUrl: 'app/comment-list.component.html',
+})
+export class CommentListComponent  { 
+    name = 'Angular'; 
+}
---
+++ src/app/comment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'comment',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css', 'css/bootstrap.min.css', 'css/stylish-portfolio.css']
+})
+export class CommentComponent  { 
+    name = 'Angular'; 
+}
