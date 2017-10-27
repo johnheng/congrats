6ed65f86282ef3c0929a0083e8c73ddfc6cccbf7
John Heng <jheng@retaildatallc.net>
2017-10-27 14:38:34 -0400

    Added images and basic CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css']
+  styleUrls: ['app/app.component.css', 'css/bootstrap.min.css', 'css/stylish-portfolio.css']
 })
 export class AppComponent  { name = 'Angular'; }
