183714a62c1fa9c8285f0ad001497c1686a959b4
John Heng <jheng@retaildatallc.net>
2017-11-07 16:26:40 -0500

    For Chris

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent }  from './app.component';
 import { CommentService } from './comment.service';
 import { CommentListComponent } from './comment-list.component';
 import { CommentComponent } from './comment.component';
-// import { Tabletop } from 'tabletop/src/tabletop';
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent, CommentComponent, CommentListComponent],
   providers:    [ 
     CommentService
-    // { provide: Tabletop, useClass: Tabletop } 
   ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/comment.service.ts
+++ src/app/comment.service.ts
@@ -1,19 +1,44 @@
 import { Injectable } from '@angular/core';
 import * as Tabletop from 'tabletop/src/tabletop';
+import { Observable } from 'rxjs/observable';
+
+import 'rxjs/add/operator/map';
+import * as bindCallback from 'rxjs/observable/bindCallback';
 
 @Injectable()
 export class CommentService {
   constructor() {}
-  private comments = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1VaWHioyj_OzKQ5J9HXDCwoSDgSRhkwbYbI9ZP_Ub2RQ&output=html';
-  
-  get() {
+  private spreadsheetUrl = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1VaWHioyj_OzKQ5J9HXDCwoSDgSRhkwbYbI9ZP_Ub2RQ&output=html';
+
+  data: Observable<Comment[]>;
+  getDataAsObservable = bindCallback.bindCallback(this.getData);
+
+  get() : Observable<{}> {
     Tabletop.init({
-      key: this.comments,
-      callback: function(data, tabletop) {
-        console.log(data);
-        return(data);
-      },
+      key: this.spreadsheetUrl,
+      callback: this.commentCallback,
       simpleSheet: true
     });
+    
+    // return this.data;
+    console.log(Tabletop);
+
+    const log = (val, cb) => cb(val);
+    const trace = bindCallback.bindCallback(log);
+    
+    // trace(Tabletop)
+    // .subscribe(x => { console.log("hello"); return x });
+    console.log(trace(Tabletop).map(res => res));
+    return trace(Tabletop).map(res => res);
+  }
+
+  commentCallback(data, tabletop) {
+    console.log(data);
+    console.log(tabletop);
+    this.data = data;
+  }
+
+  getData(tabletop) {
+    console.log(tabletop);
   }
 }
